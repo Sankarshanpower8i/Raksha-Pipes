64d05533d1de09334feccedbf744a8b94eedf355
Mr. Sankarshan Tayade <133600711+Sankarshanpower8i@users.noreply.github.com>
2024-05-16 00:10:01 +0530

    Update Sankarshan_Raksha_pipes_sql_query.sql

--- Sankarshan_Raksha_pipes_sql_query.sql
+++ Sankarshan_Raksha_pipes_sql_query.sql
@@ -102,42 +102,42 @@ SET product_name = CASE
     WHEN product_name = 'president' THEN 'LDPE-Braided Pipes'
 END;
 
-# TO combine the data
-SELECT 
-    c.customer_id, 
-    c.customer_name, 
-    c.customer_address, 
-    c.customer_contact, 
-    p.product_id, 
-    p.product_name, 
-    p.category_id, 
-    ca.category_name, 
-    s.sale_id, 
-    s.quantity, 
-    s.sale_date, 
-    e.employee_id, 
-    e.employee_name, 
-    e.department, 
-    e.designation, 
-    m.unit_id, 
-    m.unit_name, 
-    m.location AS manufacturing_unit_location, 
-    w.warehouse_id, 
-    w.warehouse_name, 
-    w.location AS warehouse_location, 
-    ex.export_id, 
-    ex.destination_country, 
-    ex.quantity AS export_quantity, 
-    ex.export_date 
-FROM 
-    Customers c 
-    LEFT JOIN Sales s ON c.customer_id = s.customer_id 
-    LEFT JOIN Products p ON s.product_id = p.product_id 
-    LEFT JOIN Categories ca ON p.category_id = ca.category_id 
-    LEFT JOIN Employees e ON e.department = 'Sales' 
-    LEFT JOIN Manufacturing_Units m ON p.category_id = m.unit_id 
-    LEFT JOIN Exports ex ON ex.product_id = p.product_id 
-    LEFT JOIN Warehouses w ON ex.destination_country = w.location;
-# then expoted to the csv 
+-- # TO combine the data
+-- SELECT 
+--     c.customer_id, 
+--     c.customer_name, 
+--     c.customer_address, 
+--     c.customer_contact, 
+--     p.product_id, 
+--     p.product_name, 
+--     p.category_id, 
+--     ca.category_name, 
+--     s.sale_id, 
+--     s.quantity, 
+--     s.sale_date, 
+--     e.employee_id, 
+--     e.employee_name, 
+--     e.department, 
+--     e.designation, 
+--     m.unit_id, 
+--     m.unit_name, 
+--     m.location AS manufacturing_unit_location, 
+--     w.warehouse_id, 
+--     w.warehouse_name, 
+--     w.location AS warehouse_location, 
+--     ex.export_id, 
+--     ex.destination_country, 
+--     ex.quantity AS export_quantity, 
+--     ex.export_date 
+-- FROM 
+--     Customers c 
+--     LEFT JOIN Sales s ON c.customer_id = s.customer_id 
+--     LEFT JOIN Products p ON s.product_id = p.product_id 
+--     LEFT JOIN Categories ca ON p.category_id = ca.category_id 
+--     LEFT JOIN Employees e ON e.department = 'Sales' 
+--     LEFT JOIN Manufacturing_Units m ON p.category_id = m.unit_id 
+--     LEFT JOIN Exports ex ON ex.product_id = p.product_id 
+--     LEFT JOIN Warehouses w ON ex.destination_country = w.location;
+-- # then expoted to the csv 
 
 
